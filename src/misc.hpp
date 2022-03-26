b37ffa983e325d44e7722bbf498578385e6c4898
payatsu <6873447+payatsu@users.noreply.github.com>
2022-03-26 19:28:15 +0900

    src/misc.hpp: add stopwatch class.

--- src/misc.hpp
+++ src/misc.hpp
@@ -2,6 +2,7 @@
 #define MISC_HPP_
 
 #include <cerrno>
+#include <chrono>
 #include <cstring>
 #include <stdexcept>
 #include <iostream>
@@ -24,6 +25,29 @@
     ERROR_BASE(message, throw std::runtime_error(std::strerror(errno)))
 
 extern const char* progname;
+
+class stopwatch{
+    using time_point = std::chrono::high_resolution_clock::time_point;
+
+public:
+    stopwatch(const std::string& description = ""): description_(description), start_(now()){}
+    ~stopwatch()
+    {
+        using std::chrono::duration_cast;
+        using std::chrono::milliseconds;
+
+        time_point end = now();
+        milliseconds elapsed_time = duration_cast<milliseconds>(end - start_);
+        std::cerr << description_ << elapsed_time.count() << " ms" << std::endl;
+    }
+
+private:
+    static time_point now(){return std::chrono::high_resolution_clock::now();}
+
+private:
+    const std::string description_;
+    const time_point start_;
+};
 
 #endif // MISC_HPP_
 
